79d4f8ff821e2760d3b33e336d7427fbd61f2179
David Yartey <yarteyd@gmail.com>
2018-06-16 02:01:04 +0000

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,11 +2,11 @@
 // TODO: Replace with your project's customized code snippet
 var firebase = require("firebase");
         var config = {
-        apiKey: "AIzaSyCwtsJU-pX7cpnNhYzdJ_4q1qSmqvVAsRI",
-        authDomain: "texttext-6728b.firebaseapp.com",
-        databaseURL: "https://texttext-6728b.firebaseio.com",
-        projectId: "texttext-6728b",
-        storageBucket: "texttext-6728b.appspot.com",
-        messagingSenderId: "986312653658"
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: ""
       };
       firebase.initializeApp(config);
